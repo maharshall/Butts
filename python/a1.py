58ddb5fe7616c00bc199d4fad7439863516d9590
maharshall <maharshall@gmail.com>
2014-02-01 11:53:31 -0500

    Assignment 1, python

--- python/a1.py
+++ python/a1.py
@@ -1,3 +1,21 @@
 def quicksort(strings):
+	size = len(strings)
+	sort(strings, 0, size-1)
 
-	#todo
+def sort(strings, start, end):
+	if(start < end):
+		pivot = strings[end]
+		i = start
+		j = end
+
+		while(i != j):
+			if(strings[i] < pivot):
+				i = i+1
+			else:
+				strings[j] = strings[i]
+				strings[i] = strings[j-1]
+				j = j-1
+
+		strings[j] = pivot
+		sort(strings, start, j-1)
+		sort(strings, j+1, end)
