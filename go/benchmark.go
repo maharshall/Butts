c98e80147ade8c6ec79e257bb34fa7330c60d357
maharshall <maharshall@gmail.com>
2014-02-05 14:28:41 -0500

    a1 Go code

--- go/benchmark.go
+++ go/benchmark.go
@@ -1,5 +1,38 @@
 package main
 
+import(
+	"fmt"
+	"os"
+	"helper"
+//	"strconv"
+	"quicksort"
+)
+
 func main() {
-	//todo
+//	if(os.Args[0] == nil){
+//		fmt.Println("File usage: <stringlen> <arraylen>")
+//	}
+
+	t1 := os.Args[0]
+	t2 := os.Args[1]
+
+	stringlen := 10 //strconv.ParseInt(t1, 0, 64)
+	arraylen := 10 //strconv.ParseInt(t2, 0, 64)
+
+	helper.ResetTimer()
+	var arr []string = helper.MakeArray(stringlen, arraylen)
+	var time  = helper.ReadTimerMilliseconds()
+	fmt.Printf("Generated %d strings of length %d in %.4f ms\n", arraylen, stringlen, time)
+    	for _, x := range(arr) {
+       	fmt.Println(x)
+    	}
+
+    	helper.ResetTimer()
+    	quicksort.sort(arr, 0, arraylen-1)
+    	time  = helper.ReadTimerMilliseconds()
+	fmt.Printf("Sorted %d strings of length %d in %.4f ms\n", arraylen, stringlen, time)
+    	for _, x := range(arr) {
+       	fmt.Println(x)
+    	}
+
 }
